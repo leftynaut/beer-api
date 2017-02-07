40eff7aac6ea18bf05708137ac4b0b1c9cc8edd1
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-02-06 21:06:03 -0600

    Style of table complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,33 @@
 angular
     .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource', 'md.data.table'])
     .controller('AppCtrl', ($scope, $http, $mdDialog) => {
-      $scope.selected = [];
-      $http({
-        method : "GET",
-        url : "https://api.punkapi.com/v2/beers"
-      }).then((response) => {
-        // if success
-        $scope.brewList = response.data;
-      }, (response) => {
-        // if error
-        $scope.brewList = response.statusText;
-      });
-      $scope.logBeer = (selection) => {
-        console.log(`${selection.name} chosen`)
-      }
-      $scope.popBeer = (selection) => {
-        console.log(selection)
-        $mdDialog.show(
-          $mdDialog.alert()
-            .parent(angular.element(document.querySelector('#popupContainer')))
-            .clickOutsideToClose(true)
-            .title(selection.name)
-            .textContent(selection.description)
-            .ariaLabel('Alert Dialog Demo')
-            .ok('Close')
-            .targetEvent(selection)
-        );
-      }
+        $scope.selected = [];
+        $http({
+            method: "GET",
+            url: "https://api.punkapi.com/v2/beers"
+        }).then((response) => {
+            // if success
+            $scope.brewList = response.data;
+        }, (response) => {
+            // if error
+            $scope.brewList = response.statusText;
+        });
+        $scope.logBeer = (selection) => {
+            console.log(`${selection.name} chosen`)
+        }
+        $scope.popBeer = (selection) => {
+            console.log(selection)
+            $mdDialog.show(
+                $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+                .title(selection.name)
+                .textContent(selection.description)
+                .ariaLabel('Alert Dialog Demo')
+                .ok('Close')
+                .targetEvent(selection)
+            );
+        }
     })
     .config(($mdThemingProvider) => {
         $mdThemingProvider.theme('docs-dark', 'default')
