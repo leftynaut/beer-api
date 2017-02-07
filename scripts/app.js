81420c971dd549e1e0973c56cd4a116e6bbb16ed
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2017-02-06 22:23:33 -0600

    Popup style

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 angular
-    .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource', 'md.data.table'])
+    .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource', 'ngSanitize', 'md.data.table'])
     .controller('AppCtrl', ($scope, $http, $mdDialog) => {
         $scope.selected = [];
         $http({
@@ -12,20 +12,28 @@ angular
             // if error
             $mdDialog.show(
                 $mdDialog.alert()
-                .clickOutsideToClose(true)
+                // .clickOutsideToClose(true)
                 .title('Error accessing the Brewdog API')
                 .textContent('Check your internet connection and try again!')
-                .ok('OK')
             );
         });
         $scope.popBeer = (selection) => {
-            console.log(selection)
+            // console.log(selection)
             $mdDialog.show(
                 $mdDialog.alert()
                 .clickOutsideToClose(true)
                 .title(selection.name)
-                .textContent(selection.description)
-                .ok('CHEERS')
+                .htmlContent(`<img class="beer-img" src=${selection.image_url}>${selection.description}<h4>Food Pairings</h4>
+                  ${selection.food_pairing.map((food, idx, arr) => {
+                  if (idx === arr.length - 1) {
+                    return ` and ${food}`;
+                  } else if (idx > 0){
+                    return ` ${food}`;
+                  } else {
+                    return food;
+                  }
+                })}`)
+                //.ok('CHEERS')
             );
         }
     })
